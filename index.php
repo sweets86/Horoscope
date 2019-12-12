8dfcf282dbcd63391d8ed30fec7524e6adc14a7e
yanica svensson <sweets86_@hotmail.com>
2019-12-12 13:12:07 +0100

    add files to the project

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include 'constant.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        <?php
+            echo Title;
+        ?>
+    </title>
+</head>
+<body>
+    
+    <?php
+    
+    $test = "asd";
+    $heh = $test;
+    echo $heh;
+
+    ?>
+</body>
+</html>
