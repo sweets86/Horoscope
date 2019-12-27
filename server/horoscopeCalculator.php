8f250a521e382a4f7774f0d376fb8bfddc82915c
yanica svensson <sweets86_@hotmail.com>
2019-12-27 22:16:56 +0100

    change language to eng

--- server/horoscopeCalculator.php
+++ server/horoscopeCalculator.php
@@ -3,18 +3,18 @@
 function getHoroscope($inputDate)
 {
     $horoscope = array(
-        "Väduren" => "03:21:04:19",
-        "Oxen" => "04:20:05:20",
-        "Tvillingarna" => "05:21:06:21",
-        "Kräftan" => "06:22:07:22",
-        "Lejonet" => "07:23:08:22",
-        "Jungfrun" => "08:23:09:22",
-        "Vågen" => "09:23:10:22",
-        "Skorpionen" => "10:23:11:21",
-        "Skytten" => "11:22:12:21",
-        "Stenbocken" => "12:22:01:19",
-        "Vattumannen" => "01:20:02:18",
-        "Fiskarna" => "02:19:03:20"
+        "Aries" => "03:21:04:19",
+        "Taurus" => "04:20:05:20",
+        "Gemini" => "05:21:06:21",
+        "Cancer" => "06:22:07:22",
+        "Leo" => "07:23:08:22",
+        "Virgo" => "08:23:09:22",
+        "Libra" => "09:23:10:22",
+        "Scorpio" => "10:23:11:21",
+        "Sagittarius" => "11:22:12:21",
+        "Capricorn" => "12:22:01:19",
+        "Aquarius" => "01:20:02:18",
+        "Pisces" => "02:19:03:20"
     );
 
     $inputDateArr = explode("-", $inputDate);
